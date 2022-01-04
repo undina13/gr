8198228dfa6dbc57382efd9a476d4be2609c773e
undina <foxundina@gmail.com>
2022-01-04 14:11:26 +0300

    swagger1

--- src/main/java/ru/undina/topjava2/web/menu/UserMenuController.java
+++ src/main/java/ru/undina/topjava2/web/menu/UserMenuController.java
@@ -2,6 +2,8 @@ package ru.undina.topjava2.web.menu;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value = UserMenuController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
+@CacheConfig(cacheNames = "usersmenu")
 public class UserMenuController {
 
      static final String REST_URL = "/api/user/menu/";
 @Autowired
     MenuRepository menuRepository;
-
+@Cacheable
     @GetMapping("/today")
     public List<Menu> findAllByToday() {
         log.info("get menu by today ");
